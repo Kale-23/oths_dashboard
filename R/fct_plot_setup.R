49cf2e973075be6b1ee15d2c0f9b5e0ed5479864
Kale-23 <kalebduch18@gmail.com>
2025-10-02 23:25:53 -0400

    UI updates and download buttons. Working pheno plot now. This should be nearly done

--- R/fct_plot_setup.R
+++ R/fct_plot_setup.R
@@ -15,6 +15,8 @@ plotly_timeseries <- function(df, new_col_name) {
     values_to = "value"
   )
 
+  df_long <- dplyr::arrange(df_long, DateTime)
+
   # Build the plot
   plotly::plot_ly(
     data = df_long,
@@ -25,13 +27,13 @@ plotly_timeseries <- function(df, new_col_name) {
     type = "scatter",
     mode = "lines+markers",
     opacity = 0.7,
-    marker = list(size = 1),
+    marker = list(size = 2),
     #fmt: skip
     text = ~ paste(
       "Location: ", series, "<br>",
       "Value: ", value
-    )
-    #connectgaps = TRUE
+    ),
+    connectgaps = TRUE
   ) |>
     plotly::layout(
       yaxis = list(title = plot_name_conversions()[[new_col_name]]),
@@ -44,16 +46,40 @@ plotly_timeseries <- function(df, new_col_name) {
 }
 
 prep_plot <- function(data, column) {
-  #TODO is this how it should be handled
+  # handle multiple columns that start with the same string
+  matched_cols <- grep(paste0("^", column, "_[0-9]*"), names(data), value = TRUE)
+  if (length(matched_cols) > 1) {
+    data <- data |>
+      dplyr::select(!dplyr::any_of(column)) |>
+      tidyr::pivot_longer(
+        cols = all_of(matched_cols),
+        names_to = "depth", # column name will become "Tsoil"
+        values_to = column,
+        names_pattern = paste0(column, "_?(.*)"),
+        names_repair = "minimal"
+      ) |>
+      dplyr::mutate(
+        !!column := ifelse(.data[[column]] == "", column, .data[[column]])
+      )
+    col_to_use <- column
+  } else {
+    # single column case
+    col_to_use <- column
+  }
+
+  # summarise and reshape depending on whether "sensor" exists
   if ("sensor" %in% colnames(data)) {
     plot_data <- data |>
-      dplyr::summarise(n = mean(.data[[column]]), .by = c(DateTime, sensor, Site)) |>
+      dplyr::summarise(
+        n = mean(.data[[col_to_use]], na.rm = TRUE),
+        .by = c(DateTime, sensor, Site, dplyr::any_of("depth"))
+      ) |>
       tidyr::pivot_wider(
-        id_cols = DateTime,
+        id_cols = c(DateTime, dplyr::matches("depth")),
         names_from = c(sensor, Site),
         values_from = n
       ) |>
-      dplyr::select(DateTime, dplyr::matches("os|prop|google")) |>
+      dplyr::select(DateTime, dplyr::matches("os|prop|google"), dplyr::everything()) |>
       dplyr::rename_with(~ gsub("os_HW", "Open Source Beech", .x)) |>
       dplyr::rename_with(~ gsub("os_SW", "Open Source Hemlock", .x)) |>
       dplyr::rename_with(~ gsub("prop_HW", "Proprietary Beech", .x)) |>
@@ -62,14 +88,27 @@ prep_plot <- function(data, column) {
       dplyr::rename_with(~ gsub("google_SW", "Google Sheets Hemlock", .x))
   } else {
     plot_data <- data |>
-      dplyr::summarise(n = mean(.data[[column]]), .by = c(DateTime, site)) |>
+      dplyr::summarise(
+        n = mean(.data[[col_to_use]], na.rm = TRUE),
+        .by = c(DateTime, Site, dplyr::any_of("depth"))
+      ) |>
       tidyr::pivot_wider(
-        id_cols = DateTime,
-        names_from = site,
+        id_cols = c(DateTime, dplyr::matches("depth")),
+        names_from = Site,
         values_from = n
       ) |>
       dplyr::rename_with(~ gsub("HW", "Beech", .x)) |>
       dplyr::rename_with(~ gsub("SW", "Hemlock", .x))
+  }
+
+  if ("depth" %in% colnames(plot_data)) {
+    plot_data <- plot_data |>
+      tidyr::pivot_wider(
+        id_cols = c(DateTime),
+        names_from = depth, # factor column
+        names_glue = "{.value} {depth}", # add suffix to all value cols
+        values_from = !c(DateTime, depth), # all except DateTime
+      )
   }
 
   plot_data
--- R/mod_global.R
+++ R/mod_global.R
@@ -14,61 +14,117 @@ mod_global_ui <- function(id) {
       sidebar = bslib::sidebar(
         width = 600,
         title = "Old Town Ecological Observatory",
-        # intro text
-        shiny::p(
-          "Visualize and download data and graphs from the Old Town Ecological Observatory, Old Town, Maine. The observatory continuously monitors near-surface climate conditions in two neighboring forest stands, beech and hemlock"
-        ),
-        # date input
-        shiny::uiOutput(ns("date_ui")),
-        # sensor input
-        shiny::checkboxGroupInput(
-          ns("sensor_selector"),
-          "Select Sensors to Include",
-          choices = c(
-            "Open Source (Arduino)" = "os",
-            "Proprietary" = "prop",
-            "Google Sheets" = "google"
+        shiny::div(
+          # intro text
+          shiny::p(
+            "Visualize and download data and graphs from the Old Town Ecological Observatory, Old Town, Maine. The observatory continuously monitors near-surface climate conditions in two neighboring forest stands, beech and hemlock"
           ),
-          selected = c("os", "prop", "google"),
-        ),
-        # site input
-        shiny::radioButtons(
-          ns("site_selector"),
-          "Select Site to Include",
-          choices = c(
-            "Both" = "both",
-            "Beech" = "hw",
-            "Hemlock" = "sw"
+          # date input
+          shiny::uiOutput(ns("date_ui")),
+          # sensor input
+          shiny::checkboxGroupInput(
+            ns("sensor_selector"),
+            shiny::span(
+              "Select Sensors to Include",
+              bslib::tooltip(
+                bsicons::bs_icon("info-circle"),
+                "Not all sensors collect all variables. If no data appears for a variable, try selecting a different sensor.",
+                placement = "right"
+              )
+            ),
+            choices = c(
+              "Open Source (Arduino)" = "os",
+              "Proprietary" = "prop",
+              "Google Sheets" = "google"
+            ),
+            selected = c("os", "prop", "google"),
           ),
-          selected = "both"
-        ),
-        # data to plot
-        shiny::checkboxGroupInput(
-          ns("plot_selector"),
-          "Select Variables to Plot",
-          choices = c(
-            "Air Temperature" = "AirT", # AirT
-            "Relative Humidity" = "RH", # RH
-            "Snow Depth" = "SnowDepth", # SnowDepth
-            #"Soil Temperature" = "sdep", # sdep
-            "Snow Water Equivalent" = "swe", # swe
-            "Frost Depth" = "fdep", # fdep
-            "Soil Temperature" = "TSoil", # TSoil_5, TSoil_25, TSoil
-            "Soil Moisture" = "VWC", # VWC_5, VWC_25, VWC
-            "Soil Specific Conductance" = "SpeCon", # SpeCon_5, SpeCon_25
-            "Phenology Greenness Index" = "gcc" # midday_gcc
-          )
-        ),
-        p(
-          "This research is supported by the National Science Foundation MacroSystems Biology
+          # site input
+          shiny::radioButtons(
+            ns("site_selector"),
+            shiny::span(
+              "Select Site to Include",
+              bslib::tooltip(
+                bsicons::bs_icon("info-circle"),
+                "Beech (HW) and Hemlock (SW)",
+                placement = "right"
+              )
+            ),
+            choices = c(
+              "Both" = "both",
+              "Beech" = "hw",
+              "Hemlock" = "sw"
+            ),
+            selected = "both"
+          ),
+          shiny::radioButtons(
+            ns("aggregate_selector"),
+            shiny::span(
+              "Aggregate Data by",
+              bslib::tooltip(
+                bsicons::bs_icon("info-circle"),
+                "Will take the mean of all data points within the selected time period, floored. This greatly speeds up plotting for long time series. Note that this will not affect the data tables below, which will show the full date and time of each observation.",
+                placement = "right"
+              )
+            ),
+            choices = c(
+              "Hour" = "hour",
+              "Day" = "day",
+              "Week" = "week",
+              "Month" = "month",
+              "None" = "none"
+            ),
+            selected = "month"
+          ),
+          # data to plot
+          shiny::checkboxGroupInput(
+            ns("plot_selector"),
+            shiny::span(
+              "Select Variables to Plot",
+              bslib::tooltip(
+                bsicons::bs_icon("info-circle"),
+                "You can select multiple variables to plot. Each variable will be shown in its own plot. If there are multiple depths for a variable, each depth will be shown as a separate line within the same plot.",
+                placement = "right"
+              )
+            ),
+            choices = c(
+              "Air Temperature" = "AirT", # AirT
+              "Relative Humidity" = "RH", # RH
+              "Snow Depth" = "SnowDepth", # SnowDepth
+              "Snow Water Equivalent" = "swe", # swe
+              "Frost Depth" = "fdep", # fdep
+              "Soil Temperature" = "TSoil", # TSoil_5, TSoil_25, TSoil
+              "Soil Moisture" = "VWC", # VWC_5, VWC_25, VWC
+              "Soil Specific Conductance" = "SpeCon", # SpeCon_5, SpeCon_25
+              "Phenology Greenness Index" = "midday_gcc" # midday_gcc
+            ),
+            selected = c("AirT")
+          ),
+          p(
+            "This research is supported by the National Science Foundation MacroSystems Biology
                 Grant #18027726"
+          )
         )
-      ),
+      ), # end sidebar
 
       shiny::uiOutput(ns("bound_plots")),
-
-      DT::dataTableOutput(ns("bound_dt")),
-      DT::dataTableOutput(ns("pheno_dt")),
+      shiny::uiOutput(ns("pheno_plots")),
+      bslib::card(
+        full_screen = TRUE, # can expand to fit screen
+        bslib::card_header(
+          "Bound Sensor Data Table"
+        ),
+        DT::dataTableOutput(ns("bound_dt")),
+        shiny::downloadButton(ns("download_bound"), "Download Filtered Bound Data")
+      ),
+      bslib::card(
+        full_screen = TRUE, # can expand to fit screen
+        bslib::card_header(
+          "Phenocam Data Table"
+        ),
+        DT::dataTableOutput(ns("pheno_dt")),
+        shiny::downloadButton(ns("download_pheno"), "Download Filtered Phenocam Data")
+      )
     )
   )
 }
@@ -84,7 +140,14 @@ mod_global_server <- function(id, bound, pheno) {
     output$date_ui <- shiny::renderUI({
       shiny::dateRangeInput(
         ns("date_selector"),
-        "Select Date Range",
+        shiny::span(
+          "Select Date Range",
+          bslib::tooltip(
+            bsicons::bs_icon("info-circle"),
+            "Date range will control all plots and data tables. You can control each plot's date range individually through the plotly toolbar.",
+            placement = "right"
+          )
+        ),
         start = min(bound$date, pheno$DateTime),
         min = min(bound$date, pheno$DateTime),
         end = max(bound$date, pheno$DateTime),
@@ -97,6 +160,7 @@ mod_global_server <- function(id, bound, pheno) {
       req(input$date_selector)
       req(input$sensor_selector)
       req(input$site_selector)
+      req(input$plot_selector)
 
       # sensor setup
       bound |>
@@ -116,26 +180,68 @@ mod_global_server <- function(id, bound, pheno) {
           } else {
             TRUE # no filter
           }
-        )
+        ) |>
+        # site filter
+        dplyr::select(c("Site", "DateTime", "sensor", dplyr::matches(input$plot_selector)))
+      # aggregate filter not done here so DTs show full dates
     })
+
+    output$download_bound <- shiny::downloadHandler(
+      filename = function() {
+        paste("oths_bound_filtered_data_", Sys.Date(), ".csv", sep = "")
+      },
+      content = function(file) {
+        req(filtered_bound())
+        readr::write_csv(filtered_bound(), file)
+      }
+    )
 
     filtered_pheno <- shiny::reactive({
       req(input$date_selector)
       pheno |>
+        # date filter
         dplyr::filter(
           DateTime >= input$date_selector[1] &
             DateTime <= input$date_selector[2]
-        )
+        ) |>
+        # column filter
+        dplyr::select(c("site", "DateTime", dplyr::matches(input$plot_selector)))
     })
+
+    output$download_pheno <- shiny::downloadHandler(
+      filename = function() {
+        paste("oths_phenocam_filtered_data_", Sys.Date(), ".csv", sep = "")
+      },
+      content = function(file) {
+        req(filtered_pheno())
+        readr::write_csv(filtered_pheno(), file)
+      }
+    )
 
     bound_plot_data <- shiny::reactive({
       req(input$plot_selector)
+      req(input$aggregate_selector)
       req(filtered_bound())
 
+      # do aggregation here so that the DTs show full dates
+      aggregated_data <- filtered_bound() |>
+        dplyr::mutate(
+          DateTime = dplyr::case_when(
+            input$aggregate_selector == "hour" ~ lubridate::floor_date(DateTime, unit = "hour"),
+            input$aggregate_selector == "day" ~ lubridate::floor_date(DateTime, unit = "day"),
+            input$aggregate_selector == "week" ~ lubridate::floor_date(DateTime, unit = "week"),
+            input$aggregate_selector == "month" ~ lubridate::floor_date(DateTime, unit = "month"),
+            TRUE ~ DateTime
+          )
+        )
+
       purrr::map(input$plot_selector, function(col) {
-        df <- prep_plot(filtered_bound(), col)
-        list(data = df, col = col)
-      })
+        if (col != "midday_gcc") {
+          df <- prep_plot(aggregated_data, col)
+          list(data = df, col = col)
+        }
+      }) |>
+        purrr::compact() # remove NULLs (ie gcc)
     })
 
     output$bound_plots <- shiny::renderUI({
@@ -144,6 +250,9 @@ mod_global_server <- function(id, bound, pheno) {
         lapply(bound_plot_data(), function(df_col) {
           df <- df_col$data
           col <- df_col$col
+
+          # handle multi col IDs (ie 5 and 25)
+
           plot_id <- paste0("plot_", col)
 
           # create plots
@@ -152,15 +261,65 @@ mod_global_server <- function(id, bound, pheno) {
           })
 
           # show plots within card for better formatting
-          div(
-            class = "plot-container",
-            bslib::card(
-              full_screen = TRUE, # can expand to fit screen
-              bslib::card_header(
-                plot_name_conversions()[[col]]
-              ),
-              plotly::plotlyOutput(ns(plot_id)) # actual plot output
-            )
+          bslib::card(
+            full_screen = TRUE, # can expand to fit screen
+            bslib::card_header(
+              plot_name_conversions()[[col]]
+            ),
+            plotly::plotlyOutput(ns(plot_id)) # actual plot output
+          )
+        })
+      )
+    })
+
+    pheno_plot_data <- shiny::reactive({
+      req(input$plot_selector)
+      req(input$aggregate_selector)
+      req(filtered_pheno())
+
+      # do aggregation here so that the DTs show full dates
+      aggregated_data <- filtered_pheno() |>
+        dplyr::mutate(
+          DateTime = dplyr::case_when(
+            input$aggregate_selector == "week" ~ lubridate::floor_date(DateTime, unit = "week"),
+            input$aggregate_selector == "month" ~ lubridate::floor_date(DateTime, unit = "month"),
+            TRUE ~ DateTime
+          )
+        ) |>
+        dplyr::rename(Site = site)
+
+      purrr::map(input$plot_selector, function(col) {
+        if (col == "midday_gcc") {
+          df <- prep_plot(aggregated_data, col)
+          list(data = df, col = col)
+        }
+      }) |>
+        purrr::compact() # remove NULLs (ie != gcc)
+    })
+
+    output$pheno_plots <- shiny::renderUI({
+      req(pheno_plot_data())
+      tagList(
+        lapply(pheno_plot_data(), function(df_col) {
+          df <- df_col$data
+          col <- df_col$col
+
+          # handle multi col IDs (ie 5 and 25)
+
+          plot_id <- paste0("bound_plot_", col)
+
+          # create plots
+          output[[plot_id]] <- plotly::renderPlotly({
+            plotly_timeseries(df, col)
+          })
+
+          # show plots within card for better formatting
+          bslib::card(
+            full_screen = TRUE, # can expand to fit screen
+            bslib::card_header(
+              plot_name_conversions()[[col]]
+            ),
+            plotly::plotlyOutput(ns(plot_id)) # actual plot output
           )
         })
       )
@@ -172,6 +331,7 @@ mod_global_server <- function(id, bound, pheno) {
         options = list(pageLength = 5, scrollX = TRUE)
       )
     })
+
     output$pheno_dt <- DT::renderDT({
       DT::datatable(
         filtered_pheno(),
